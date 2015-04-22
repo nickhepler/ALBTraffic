8d0735473953b8098b7ca54d759aebf2fee3f946
Nick Hepler <nick.hepler@outlook.com>
2015-04-21 21:48:39 -0400

    Updated download scripts to 0.1.0

--- scripts/download_mva_linux.R
+++ scripts/download_mva_linux.R
@@ -1,6 +1,6 @@
-#  format_data_linux.R
+#  download_mva_linux.R
 #
-#  Version 0.0.1
+#  Version 0.1.0
 #
 #  Copyright 2015 Nick Hepler <nhepler@albany.edu>
 #
@@ -20,17 +20,15 @@
 #  MA 02110-1301, USA.
 #
 #
-library("dplyr")
-
 if (!file.exists("data")) {
   dir.create("data")
 }
 
-OutputFile <- file("output.txt") #  Create output file.
+OutputFile <- file("./data/output.txt") #  Create output file.
 
 #  Set URL and destination of file to download
 file.mva.url <- "https://data.ny.gov/api/views/e8ky-4vqe/rows.csv?accessType=DOWNLOAD"
-file.mva.dest <- "./data/Motor_Vehicle_Crashes_-_Case_Information__Beginning_2009.csv"
+file.mva.dest <- "./data/raw_mva.csv"
 
 download.file(file.mva.url,
               destfile = file.mva.dest, 
@@ -38,28 +36,25 @@ download.file(file.mva.url,
 
 file.mva.timestamp <- date() #  Write timestamp
 
-writeLines(c("Downloaded AADT data :: ",
+writeLines(c("Downloaded MVA data :: ",
            file.mva.timestamp),
            OutputFile) #  Write timestamp to output file.
 close(OutputFile)
 
 #  Remove temporary variables from environment.
-rm(file.mva.url, file.mva.dest, file.mva.timestamp)
+rm(file.mva.url, file.mva.dest, file.mva.timestamp, OutputFile)
 
 list.files("./data") #  List files.
 
-#  Load data into global environment, REMOVE once tidy data has been scripted
-#  and tested.
+#  Load data into global environment.
 raw.mva <- read.csv(
-  "./data/Motor_Vehicle_Crashes_-_Case_Information__Beginning_2009.csv",
+  "./data/raw_mva.csv",
   stringsAsFactors=FALSE)
 
-#  Format the data.
-#  Convert Date variable from factor to date data type.
-raw.mva$Date <- as.Date(raw.mva$Date, "%m/%d/%Y")
-raw.mva$Crash.Descriptor <- as.factor(raw.mva$Crash.Descriptor)
-raw.mva$Day.of.Week <- as.factor(raw.mva$Day.of.Week)
-raw.mva <- subset(raw.mva, Municipality == "ALBANY")
+#  Subset the data now to avoid Github upload limit
+raw.mva <- subset(raw.mva, Municipality == "ALBANY") 
 
-#  Subset the data.
-mva <- tbl_df(raw.mva)
+#  Write the formated file to CSV
+write.csv(raw.mva, file = "./data/subset_mva.csv")
+
+source("./scripts/format_mva.R")
